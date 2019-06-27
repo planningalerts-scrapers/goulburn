980e156a2280252a724f60ff59e4a858c791004f
Matthew Landauer <mlandauer@gmail.com>
2019-06-27 16:40:20 +1000

    Update to latest gem

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,3 @@
 require "technology_one_scraper"
 
-TechnologyOneScraper.scrape_and_save_period(
-  url: "https://eservices.goulburn.nsw.gov.au/eServicesProd",
-  period: "TW"
-)
+TechnologyOneScraper.scrape_and_save(:goulburn)
